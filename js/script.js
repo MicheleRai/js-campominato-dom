d5ad9185ef2448aca44866a9729b1fbf35182f0f
MicheleRai <rainonemichele1999@gmail.com>
2022-11-03 14:48:47 +0100

    base

--- js/script.js
+++ js/script.js
@@ -8,3 +8,52 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;*/
+
+const eleSelectLevel = document.querySelector('#select-level');
+const eleBtnPlay = document.querySelector('#btn-play');
+const eleBtnHelp = document.querySelector('#btn-help');
+const eleStartScreen = document.querySelector('.start-screen');
+const eleGrid = document.querySelector('.grid');
+
+eleBtnPlay.addEventListener('click', function () {
+
+	eleGrid.innerHTML = '';
+
+	eleGrid.classList.remove('hidden');
+	eleStartScreen.classList.add('hidden');
+
+	const nCells = parseInt(eleSelectLevel.value);
+
+	const sideSquare = Math.sqrt(nCells);
+	eleGrid.style.setProperty('--sideSquare', sideSquare);
+
+	for (let i = 1; i <= nCells; i++) {
+
+		const eleCell = document.createElement('div');
+		eleCell.classList.add('cell');
+		eleCell.innerHTML = i;
+		
+		eleGrid.append(eleCell);
+
+		eleCell.addEventListener('click', toggleCell);
+	}
+});
+
+eleBtnHelp.addEventListener('click', function () {
+	if (eleBtnHelp.dataset.function == 'show-help') {
+		eleBtnHelp.innerHTML = 'Back to game';
+		eleBtnHelp.dataset.function = 'show-game';
+		eleGrid.classList.add('hidden');
+		eleStartScreen.classList.remove('hidden');
+	} else if (eleBtnHelp.dataset.function == 'show-game') {
+		eleBtnHelp.innerHTML = 'Show help';
+		eleBtnHelp.dataset.function = 'show-help';
+		eleGrid.classList.remove('hidden');
+		eleStartScreen.classList.add('hidden');
+	}
+});
+
+
+function toggleCell() {
+	this.classList.toggle('active');
+}
